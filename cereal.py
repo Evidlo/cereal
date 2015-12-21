f6a8694217c9d176a7342a6d08e69d9c8aa749d4
ieee <ieee@volt.ecn.purdue.edu>
2015-12-20 21:52:11 -0500

    added content processors

--- cereal.py
+++ cereal.py
@@ -2,11 +2,9 @@
 # Evan Widloski - 2015-12-12
 # Cereal - Minimal static website generator
 
-#issues - how to do template inheritance
-
 import yaml
 import mistune
-import os, shutil, errno
+import os, sys, shutil, errno
 from jinja2 import Environment, FileSystemLoader, Template
 
 # useful exceptions
@@ -19,7 +17,43 @@ symlink = False
 
 env = Environment(loader=FileSystemLoader('.'))
 
-#make all dirs up to path
+#------------- Content Processors --------------
+
+def join_constructor(loader,node):
+    value = loader.construct_sequence(node)
+    return '\n'.join(value)
+
+def md_constructor(loader,node):
+    value = loader.construct_scalar(node)
+    return mistune.markdown(value,escape=False)
+
+def jinja_constructor(loader,node):
+    value = loader.construct_scalar(node)
+    value = "{% extends 'layout/macros.html' %} {% block _macro_ %}" \
+        + value \
+        + "{% endblock %}"
+    rend = env.from_string(value).render()
+    return rend
+
+def py_constructor(loader,node):
+    from cStringIO import StringIO
+    value = loader.construct_scalar(node)
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    exec(value)
+    sys.stdout = old_stdout
+    return redirected_output.getvalue()
+
+# add content tags
+yaml.add_constructor(u'!join',join_constructor)
+yaml.add_constructor(u'!md',md_constructor)
+yaml.add_constructor(u'!j2',jinja_constructor)
+yaml.add_constructor(u'!py',py_constructor)
+
+#-----------------------------------------
+
+
+# make all dirs up to path
 def make_path(path):
     try:
         os.makedirs(path)
@@ -28,7 +62,7 @@ def make_path(path):
             pass
         else: raise
 
-#copy or symlink a file
+# copy or symlink a file
 def copy_resource(source,dest):
     try:
         if symlink:
@@ -40,7 +74,7 @@ def copy_resource(source,dest):
             os.remove(dest)
             copy_resource(source, dest)
 
-#iterate through all files in content_dir
+# iterate through all files in content_dir
 for subdir, dirs, files in os.walk(content_dir):
     for file in files:
 
@@ -49,17 +83,17 @@ for subdir, dirs, files in os.walk(content_dir):
         output_file = "%s/%s" % (output_dir, os.path.relpath(content_file,content_dir))
         make_path(os.path.dirname(output_file))
 
-        #if this is a yaml file, do site rendering
+        # if this is a yaml file, do site rendering
         if file.endswith('.yaml'):
             #rename output .html
             output_file = os.path.splitext(output_file)[0] + '.html'
 
-            #open content file, find layout and render it
+            # open content file, find layout and render it
             with open(content_file) as f:
                 yaml_data = yaml.load(f)
 
-                #try to find the layout specified on content_file
-                #if not specified, print a warning and skip content_file
+                # try to find the layout specified on content_file
+                # if not specified, print a warning and skip content_file
                 try:
                     layout_file = "%s/%s" % (layout_dir, yaml_data['layout'])
 
@@ -67,13 +101,8 @@ for subdir, dirs, files in os.walk(content_dir):
                     print('No layout specified for %s. Skipping...' % content_file)
                     break
 
-                # process content as jinja2 and markdown before rendering
-                if 'content' in yaml_data:
-                    yaml_data['content'] = env.from_string(yaml_data['content']).render()
-                    yaml_data['content'] = mistune.markdown(yaml_data['content'],escape=False)
-
-
-                # create path to output_file and render it. if layout not found, print warning
+                # create path to output_file and render it.
+                # if layout not found, print warning
                 try:
                     template = env.get_template(layout_file)
                 except TemplateNotFound:
@@ -85,6 +114,6 @@ for subdir, dirs, files in os.walk(content_dir):
                 with open(output_file,'w+') as out:
                     out.write(template.render(**yaml_data))
 
-        #if this is not yaml, just copy the file
+        # if this is not yaml, just copy the file
         else:
             copy_resource(content_file,output_file)
